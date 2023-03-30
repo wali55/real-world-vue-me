4aeb4fbdb20a8c44bc2045c6109e8fe6821d95b0
wali55 <walisantunu@gmail.com>
2023-03-30 20:43:03 +0600

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null
+    }
+  },
+  created() {
+    axios.get('https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events').then(response => {
+      this.events = response.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
